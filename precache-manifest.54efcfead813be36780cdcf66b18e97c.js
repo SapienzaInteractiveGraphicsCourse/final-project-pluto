57dbe102e2138a0db42fb62eb8e787dca98c6722
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2020-07-26 12:29:41 +0200

    Updates

---
+++ precache-manifest.54efcfead813be36780cdcf66b18e97c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6938522acc93e8ae4a53f6a25ea5d0",
+    "url": "/final-project-pluto/index.html"
+  },
+  {
+    "revision": "628a1be752283bbe9a31",
+    "url": "/final-project-pluto/static/css/main.0604f994.chunk.css"
+  },
+  {
+    "revision": "31fca355ea39e170556f",
+    "url": "/final-project-pluto/static/js/2.f75ae928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-project-pluto/static/js/2.f75ae928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628a1be752283bbe9a31",
+    "url": "/final-project-pluto/static/js/main.d3dfd4ee.chunk.js"
+  },
+  {
+    "revision": "11b23d7cc7a4a4879c43",
+    "url": "/final-project-pluto/static/js/runtime-main.8cb5db06.js"
+  },
+  {
+    "revision": "11fdb98f3daa502e19a24e91d91ccbe9",
+    "url": "/final-project-pluto/static/media/10450_Rectangular_Grass_Patch_v1_Diffuse.11fdb98f.jpeg"
+  },
+  {
+    "revision": "27d19274cf609012cc18eba41ebcbf99",
+    "url": "/final-project-pluto/static/media/10450_Rectangular_Grass_Patch_v1_iterations-2.27d19274.mtl"
+  },
+  {
+    "revision": "972d22eb2adbea6d49e6deba81d3c857",
+    "url": "/final-project-pluto/static/media/10450_Rectangular_Grass_Patch_v1_iterations-2.972d22eb.obj"
+  },
+  {
+    "revision": "bf6e2636b008172a9340c96c0e0c4cea",
+    "url": "/final-project-pluto/static/media/Fence.bf6e2636.obj"
+  },
+  {
+    "revision": "1a7dd781df83f4293d5baac4d9efefee",
+    "url": "/final-project-pluto/static/media/Thai_house.1a7dd781.obj"
+  },
+  {
+    "revision": "382176cdbe81e997d51d6d69d0987025",
+    "url": "/final-project-pluto/static/media/bark.382176cd.png"
+  },
+  {
+    "revision": "358102cb0d99267b347eecf9514ef5db",
+    "url": "/final-project-pluto/static/media/creme.358102cb.jpg"
+  },
+  {
+    "revision": "a3430e058e27c12ebf4d3bcb937cc06a",
+    "url": "/final-project-pluto/static/media/grass.a3430e05.jpg"
+  },
+  {
+    "revision": "fb599f5d2506305ba3ed02a3979a413d",
+    "url": "/final-project-pluto/static/media/leaf.fb599f5d.png"
+  },
+  {
+    "revision": "8397dda6f57796da8d71711b720246f4",
+    "url": "/final-project-pluto/static/media/shirt.8397dda6.jpg"
+  },
+  {
+    "revision": "06eb0b4774cd42c6291c3f34aa6edecb",
+    "url": "/final-project-pluto/static/media/skinTexture.06eb0b47.jpg"
+  },
+  {
+    "revision": "a6e38cb058827532d213de0a26f240ce",
+    "url": "/final-project-pluto/static/media/tree2.a6e38cb0.obj"
+  }
+]);
